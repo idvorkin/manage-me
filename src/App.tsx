dafb555e85821ea90ec4d64f82f6d38c07d7cf2b
Igor Dvorkin <idvorkin@gmail.com>
2018-06-09 15:55:27 -0700

    w00t! I can use multiple files with VSCode refactoring.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { CalendarHelper } from "./CalendarHelper";
+
 // @flow
 import React from 'react';
 import { Button, StyleSheet, Text, View, Clipboard } from 'react-native';
@@ -5,41 +7,45 @@ import Expo from 'expo';
 import moment from 'moment';
 import { Linking } from 'react-native';
 
+export type ICalendarEvent = Expo.Calendar.Event;
+
 interface IAppProps {
-  calendarEvent:any[]
 }
 
 interface IAppState {
-  calendarEvents: any[]
+  calendarEvents: ICalendarEvent[]
 }
 
 class AppState implements IAppState {
-  constructor(public calendarEvents: any[]) {
+  constructor(public calendarEvents: ICalendarEvent[]) {
   }
 }
 
+
 export default class App extends React.Component<IAppProps, IAppState> {
-  constructor(props:any) {
+  constructor(props: any) {
     super(props)
     this.state = new AppState([])
   }
-    async componentWillMount() {
-        // TBD Figure out state
-        await this.loadCalendarEvents()
-        console.log('component will mount TS++')
-        console.log('component will mount--')
+  calendarHelper = new CalendarHelper()
+  async componentWillMount() {
+    // TBD Figure out state
+    console.log('component will mount TSX2++')
+    await this.loadCalendarEvents()
+    console.log('component will mount--')
+  }
 
-    }
-    async loadCalendarEvents()
-    {
-        const calendarEvents = await this.getCalendarEvents()
-        this.setState(
-            {
-                // copy to object to avoid what I think is a promise reference or some such.
-                "calendarEvents": calendarEvents
-            }
-        )
-    }
+  async loadCalendarEvents() {
+    const start = moment().startOf('day') as any;
+    // const end = moment().endOf('day');
+    const end = moment().add(3,'days') // Debugging set forward a few days.
+
+    this.setState({
+        // Moment typing needs fixing.
+          "calendarEvents": await this.calendarHelper.getCalendarEvents(start as any, end as any)
+      }
+    )
+  }
 
   openNotabilityWithGratefulTitleOnClipboard() {
     const title = `Grateful ${moment().format('LL')}`
@@ -48,100 +54,45 @@ export default class App extends React.Component<IAppProps, IAppState> {
     Linking.openURL('notability://junk')
   }
 
-  calendarEventToString(calendarEvent:any) {
-    const e = calendarEvent
-    const start = moment(e.startDate)
-    return `${start.format('LT')} - ${e.title}`
+
+  renderStartTimeAndTitle(calendarEvent: any) {
+    return <View key={calendarEvent.id} style={styles.agendaContainer}>
+      <Text style={styles.agendaHour}> {moment(calendarEvent.startDate).format('h:mm A')}:</Text>
+      <Text>{(calendarEvent.title)}</Text>
+    </View>
   }
 
-  async ensureCalendarPermissions()
-  {
-    console.log("Asking for permissions")
-    const perm = "calendar" // Looks like there isn't a permissions enum (need to log a bug)
-    const { Permissions } = Expo;
-    console.log(perm)
-    const { status } = await Permissions.askAsync(perm);
-    console.log(`Permission returned ${status}`)
-    if (status !== 'granted') {
-      alert('Hey! You might want to enable notifications for my app, they are good.');
-    }
-  }
+  render() {
+    console.log(`render++`)
 
-  async getCalendarEvents()
-  {
+    const cleanEvents = this.calendarHelper.stripNoisyEvents(this.state.calendarEvents);
+    //  Agenda component
+    const agendaComponent = cleanEvents
+      .map(this.renderStartTimeAndTitle)
 
-    this.ensureCalendarPermissions()
-
-    let eventsToReturn:any[] = []
-    const calendars = await Expo.Calendar.getCalendarsAsync()
-
-    // TBD Remove all day events as they make it hard to see.
-    // TBD can probably do this with a map reduce
-
-    const tomorrow:any = moment().endOf('day');
-    // const tomorrow = moment("2018-06-12") // Debugging set a random day
-    for (let cal of calendars) {
-      const events = await Expo.Calendar.getEventsAsync([(cal.id) as string], (moment().startOf('day') as any), tomorrow)
-      eventsToReturn = eventsToReturn.concat(events)
-    }
-    return eventsToReturn
-  }
-  async copyAgendaToClipboard() {
-    const calendarEvents  =  this.state.calendarEvents
-    let output = `Agenda for ${moment().format('LL')}\n`
-    const eventStrings = calendarEvents.map(this.calendarEventToString).join('\n')
-    output += eventStrings
-    Clipboard.setString(output)
-  }
-
-    stripAmazonConferenceRoomJunk(location:string)
-    {
-        return location.replace("CONF US SEA ","").replace("AV/VC","");
-    }
-
-    cleanedCalenderEvents()
-    {
-        return this.state.calendarEvents
-               .filter(e=>!e.title.startsWith('Canceled:'))
-    }
-
-    renderStartTimeAndTitle(calendarEvent:any)
-    {
-            return <View key={calendarEvent.id} style={styles.agendaContainer}>
-            <Text style={styles.agendaHour}> {moment(calendarEvent.startDate).format('h:mm A')}:</Text>
-            <Text>{(calendarEvent.title)}</Text>
-            </View>
-    }
-
-    render() {
-        console.log(`render++`)
-
-        //  Agenda component
-        const agendaComponent = this.cleanedCalenderEvents().map(this.renderStartTimeAndTitle)
-
-        //  Next Meeting component
-        const nextMeetingComponent = this.cleanedCalenderEvents()
-            .filter(e=>moment(e.startDate) > moment().subtract(1,'hours')).slice(0,3)
-            .map(nextMeeting=>
+    //  Next Meeting component
+    const nextMeetingComponent = cleanEvents
+      .filter(e => moment(e.startDate) > moment().subtract(1, 'hours')).slice(0, 3)
+      .map(nextMeeting =>
         <View key={nextMeeting.id}>
-                {this.renderStartTimeAndTitle(nextMeeting)}
-                <Text> {this.stripAmazonConferenceRoomJunk(nextMeeting.location)}</Text>
+          {this.renderStartTimeAndTitle(nextMeeting)}
+          <Text> {this.calendarHelper.stripAmazonConferenceRoomJunk(nextMeeting.location || "")}</Text>
         </View>);
 
-        return (
-            <View style={styles.container} >
-            <Text style={styles.dayText}>It is {moment().format('LL - LT')}</Text>
-            {agendaComponent}
-            <Button title="Open notability with Grateful"
-            onPress={() => this.openNotabilityWithGratefulTitleOnClipboard()} />
-            <Button title="Refresh" onPress={() => this.loadCalendarEvents()} />
-            <Button title = "Random Wisdom" onPress={() => Linking.openURL('http://idvorkin.github.io/random')}/>
-            <Text style={styles.dayText}>Upcoming meetings</Text>
-            {nextMeetingComponent}
-            <Text style={styles.dayText}>Be Disciplined And Deliberate Every Day</Text>
-            </View>
-        );
-    }
+    return (
+      <View style={styles.container} >
+        <Text style={styles.dayText}>It is {moment().format('LL - LT')}</Text>
+        {agendaComponent}
+        <Button title="Open notability with Grateful"
+          onPress={() => this.openNotabilityWithGratefulTitleOnClipboard()} />
+        <Button title="Refresh" onPress={() => this.loadCalendarEvents()} />
+        <Button title="Random Wisdom" onPress={() => Linking.openURL('http://idvorkin.github.io/random')} />
+        <Text style={styles.dayText}>Upcoming meetings</Text>
+        {nextMeetingComponent}
+        <Text style={styles.dayText}>Be Disciplined And Deliberate Every Day</Text>
+      </View>
+    );
+  }
 }
 
 // Styles tutorial
---
+++ src/CalendarHelper.tsx
@@ -0,0 +1,44 @@
+import Expo from 'expo';
+import moment from 'moment';
+import {  ICalendarEvent, } from "./App";
+
+export class CalendarHelper {
+  async ensureCalendarPermissions() {
+    console.log("Asking for permissions");
+    const perm = "calendar"; // Looks like there isn't a permissions enum (need to log a bug)
+    const { Permissions } = Expo;
+    console.log(perm);
+    const { status } = await Permissions.askAsync(perm);
+    console.log(`Permission returned ${status}`);
+    if (status !== 'granted') {
+      alert('Hey! You might want to enable notifications for my app, they are good.');
+    }
+  }
+  async getCalendarEvents(start: Date, end: Date) {
+    this.ensureCalendarPermissions();
+    let eventsToReturn: ICalendarEvent[] = [];
+    const calendars = await Expo.Calendar.getCalendarsAsync();
+    // TBD Remove all day events as they make it hard to see.
+    // When will JS/TS get an async select many
+    // Hymn - isn't that RxJS? 
+    for (let cal of calendars) {
+      const events = await Expo.Calendar.getEventsAsync([cal.id as string], start, end);
+      eventsToReturn = eventsToReturn.concat(events);
+    }
+    return eventsToReturn;
+  }
+  stripAmazonConferenceRoomJunk(location: string) {
+    return location.replace("CONF US SEA ", "").replace("AV/VC", "");
+  }
+  stripNoisyEvents(events: ICalendarEvent[]) {
+    return events
+      .filter(e => !(e.title || "").startsWith('Canceled:'));
+    // Potentially remove all day events
+    // TBD  - can I modify a field when returning it? E.g. removing conf room number? Ask JS expert.
+  }
+  calendarEventToString(calendarEvent: ICalendarEvent) {
+    const e = calendarEvent;
+    const start = moment(e.startDate);
+    return `${start.format('LT')} - ${e.title}`;
+  }
+}
